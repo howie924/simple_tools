7440c1443ffcff599950981cbbcc494213281361
Howie <haowenwu@ymail.com>
2020-04-03 15:13:04 +0800

    convert picture format
    Convert a picture folder to any desired format cv2 supports. Output path is the called Converted under the original folder.

---
+++ pic-fold-convert.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 13:56:21 2020
+
+@author: Administrator
+"""
+
+import os
+
+path = r'C:\Users\Administrator\Desktop\齿痕'
+imglist = []
+imgnamelist = []
+for img in os.listdir(path):
+    imglist.append(os.path.join(path, img))
+    imgnamelist.append(img)
+
+for i in range(len(imglist)):
+    os.rename(imglist[i],os.path.join(path, str(i+909 ) + '.jpg'))
+    
+    
+
+import os
+import cv2
+
+
+def imgconvertImgFormat(formatTo, dirpath):
+    imglist = []
+    imgNamelist = []
+    
+    
+    for img in os.listdir(dirpath):
+        fp = os.path.join(dirpath, img)
+        if os.path.isfile(fp):
+            imglist.append(fp)
+            imgNamelist.append(img)
+
+    
+    dirpathout = dirpath + '\Converted'
+    os.mkdir(dirpathout)
+    
+
+    Tolist = []
+    for img in imgNamelist:
+        name, format = img.split('.')
+        Tolist.append(os.path.join(dirpathout, name + formatTo))
+    
+    for imgs, outpath in zip(imglist,Tolist):
+        img = cv2.imread(imgs)
+        cv2.imwrite(outpath,img)
+           
+if __name__=="__main__":
+    imgconvertImgFormat('.png',  r'C:\Users\Administrator\Desktop\chihen')
