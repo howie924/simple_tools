bcfd4b9f1248bea66cb19beba1066bc37a34b6bf
Howie <haowenwu@ymail.com>
2020-07-23 15:28:36 +0800

    Add files via upload

---
+++ findbycountlist.py
@@ -0,0 +1,9 @@
+#find element which counts = k in a give list
+def(list, k):
+
+    oneindexlist = []
+    s = set(List)
+    for i in s:
+        if List.count(i) == k:
+            oneindexlist.append(List.index(i))
+    return oneindexlist
